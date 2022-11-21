0e8274e07d7284f3a63646edd84b8753af0cc5c8
riswanda21 <albertcastell12@gmail.com>
2022-11-21 12:18:51 +0800

    Update

--- app/src/main/assets/app.js
+++ app/src/main/assets/app.js
@@ -1,8 +1,13 @@
 route('/', 'home.html');
 route('/kesehatan', 'kesehatan.html');
+route('/wisata-view', 'wisata-view.html');
 function main() {
     let parsedata = new Http();
     let app = new App();
+    var parameter = location.hash.split("/");
+    if(parameter[1] == "kesehatan" && parameter[2]) {
+      parsedata.get('view','views/kesehatan-view.html');
+    }
 }
 function startSearch() {
     return {
@@ -20,4 +25,22 @@ function startSearch() {
       },
     };
   }
+  function getHospitalDetails() {
+    var parameter = location.hash.split("/");
+    return {
+      dataSearch: parameter[2],
+      dataRumahSakit1: null,
+      isLoading: false,
+      fetchRumahSakit1() {
+        this.isLoading = true;
+        fetch('https://rs-bed-covid-api.vercel.app/api/get-bed-detail?hospitalid=' + parameter[2] + '&type=2')
+          .then((res) => res.json())
+          .then((data) => {
+            this.isLoading = false;
+            this.dataRumahSakit1 = data;
+          });
+      },
+    };
+  }
+  
 ready(main);
