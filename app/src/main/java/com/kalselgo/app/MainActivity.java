6da06d63e7913cb95750050cda2bfa8f9088bb9c
riswanda21 <albertcastell12@gmail.com>
2022-11-24 03:41:11 +0800

    Update

--- app/src/main/java/com/kalselgo/app/MainActivity.java
+++ app/src/main/java/com/kalselgo/app/MainActivity.java
@@ -19,8 +19,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.activity_main);
+	    setContentView(R.layout.activity_main);
 		
 		WebView webView = (WebView) findViewById(R.id.web);
 		
