676bcb0c76c0cf1b8fcd34f5a9ccb9edc1d7de0b
riswanda21 <albertcastell12@gmail.com>
2022-11-26 10:51:50 +0800

    Update

--- app/src/main/assets/app.js
+++ app/src/main/assets/app.js
@@ -165,4 +165,15 @@ function getWisataDetails() {
         },
     };
 }
+function add3Dots(string, limit)
+{
+  var dots = "...";
+  if(string.length > limit)
+  {
+    // you can also use substr instead of substring
+    string = string.substring(0,limit) + dots;
+  }
+ 
+    return string;
+}
 ready(main);
